5b9591468cc1f316a7139f8429661afe596f7655
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-01-25 17:09:59 +0530

    Initial changes

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var program = new interfaces.ExcerciseTwo();
+            program.run();
+        }
+    }
+}
