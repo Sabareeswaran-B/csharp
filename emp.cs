cb320e9fff7d87008ebd852001a0587dd3b1ca3a
Sabareeswaran B <sabareeswaran2510@gmail.com>
2022-02-10 09:04:54 +0530

    employee clas

---
+++ emp.cs
@@ -0,0 +1,37 @@
+using System;
+namespace EmployeeExample
+{
+    public abstract class Emp
+    {
+        public abstract string getEmpName();
+        public abstract int getEmpSalary(); 
+    }
+
+    public class Employee : Emp
+    {
+        public string name {get; set;}
+        public int salary {get; set;}
+
+        public Employee(string name, int salary){
+            this.name = name;
+            this.salary = salary;
+        }
+        public override string getEmpName()
+        {
+            return name;
+        }
+        public override int getEmpSalary()
+        {
+            return salary;
+        }
+    }
+
+    // public class MainEmployee
+    // {
+    //     public static void Main(){
+    //         Employee objEmp = new Employee("Kanishkar", 50000);
+    //         Console.WriteLine("The name of the employee is: {0}", objEmp.getEmpName());
+    //         Console.WriteLine("The salary of the employee is: {0}", objEmp.getEmpSalary());            
+    //     }
+    // }
+}
